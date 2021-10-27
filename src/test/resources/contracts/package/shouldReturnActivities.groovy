a367776560f5049f1f0fe521d95d0855270dbdaa
emanueltrandafir1993 <emanueltrandafir1993@gmail.com>
2021-10-27 15:13:37 +0300

    added client app

--- src/test/resources/contracts/package/shouldReturnActivities.groovy
+++ src/test/resources/contracts/package/shouldReturnActivities.groovy
@@ -5,11 +5,14 @@ Contract.make {
     description "should return activities"
     request{
         method GET()
-        url("api/activities") {
+        url("/api/activities") {
         }
     }
     response {
         body("[{\"id\":4,\"name\":null,\"type\":\"CYCLING\",\"durationInMinutes\":30},{\"id\":5,\"name\":null,\"type\":\"SPIKEBALL\",\"durationInMinutes\":60},{\"id\":6,\"name\":null,\"type\":\"SPIKEBALL\",\"durationInMinutes\":90}]")
         status 200
+        headers {
+            header 'Content-Type': 'application/json'
+        }
     }
 }
